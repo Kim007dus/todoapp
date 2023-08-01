c61745b40b6a5cf4e214ea2c741e0c7b22d7abaf
Kim007dus <kim.raven@gmail.com>
2023-08-01 12:06:02 +0200

    add addtodo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,15 @@
 import {useState} from "react";
+import Addtodo from "./components/addtodo.jsx";
+
 
 
 function App() {
-    const [formState, setFromState] = useState({
-        title: "",
-        status: "",
-        priority: "",
-    })
 
-    function handleChange(e) {
-        setFromState({
-            ...formState,
-            [e.target.name]: e.target.value
-        });
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(formState)
-    }
 
     return (
         <>
             <h1>To Do App</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="addtask">Title to do
-                    <input
-                        type="text"
-                        value={formState.title}
-                        id="addtask"
-                        name="title"
-                        placeholder="Add the title of your to-do"
-                        onChange={handleChange}
-                    />
-                </label>
-                <label htmlFor="selectstatus">
-                    Select status:
-                    <select
-                        id="selectstatus"
-                        name="status"
-                        value={formState.status}
-                        onChange={handleChange}>
-                        <option value="Done">Done</option>
-                        <option value="Busy with it">Busy with it</option>
-                        <option value="Need to start">Need to start</option>
-                    </select>
-                </label>
-
-                <label htmlFor="selectpriority">
-                    Select Priority
-                    <select
-                        id="selectpriority"
-                        name="priority"
-                        value={formState.priority}
-                        onChange={handleChange}>
-                        <option value="Today">Today</option>
-                        <option value="This week">This week</option>
-                        <option value="This month">This month</option>
-                    </select>
-                </label>
-                <button type="submit">Add to your list</button>
-            </form>
+          <Addtodo />
 
 
         </>
--- src/components/addtodo.js
+++
@@ -1,6 +0,0 @@
-import {useState} from "react";
-
-function Addtodo () {
-
-
-    export default Addtodo
